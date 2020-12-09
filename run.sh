a16c66c7e72df685799a99f97c3709bcd4b5e930
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2020-12-09 19:36:17 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "-- Building and starting Necronda Server..."
-make update && make compile && \
+make compile && \
  echo -e "-- Successfully finished compiling!\n" && \
  sleep 0.0625 && \
  echo -e "-- Starting Server...\n" && \
