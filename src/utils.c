8ac29b38154c372c54a0a3793f85b064ddba7bbf
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2020-12-12 17:14:59 +0100

    Fixing format_duration

--- src/utils.c
+++ src/utils.c
@@ -12,7 +12,7 @@ char *format_duration(unsigned long micros, char *buf) {
         sprintf(buf, "%.1f ms", (double) micros / 1000);
     } else if (micros < 1000000) {
         sprintf(buf, "%li ms", micros / 1000);
-    } else if (micros < 100000000) {
+    } else if (micros < 60000000) {
         sprintf(buf, "%.1f s", (double) micros / 1000000);
     } else if (micros < 6000000000) {
         sprintf(buf, "%.1f min", (double) micros / 1000000 / 60);
