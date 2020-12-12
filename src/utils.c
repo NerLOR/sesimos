38448401d0325fc64ef133894ee2f36ec9660e11
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2020-12-12 15:31:26 +0100

    Updated format_duration

--- src/utils.c
+++ src/utils.c
@@ -12,10 +12,8 @@ char *format_duration(unsigned long micros, char *buf) {
         sprintf(buf, "%.1f ms", (double) micros / 1000);
     } else if (micros < 1000000) {
         sprintf(buf, "%li ms", micros / 1000);
-    } else if (micros < 10000000) {
-        sprintf(buf, "%.1f s", (double) micros / 1000000);
     } else if (micros < 100000000) {
-        sprintf(buf, "%li s", micros / 1000000);
+        sprintf(buf, "%.1f s", (double) micros / 1000000);
     } else if (micros < 6000000000) {
         sprintf(buf, "%.1f min", (double) micros / 1000000 / 60);
     } else {
