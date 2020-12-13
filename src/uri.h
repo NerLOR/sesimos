db94388e6973293ae397f29228e1200d200271a7
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2020-12-13 14:12:41 +0100

    Added struct uri

--- src/uri.h
+++ src/uri.h
@@ -8,4 +8,16 @@
 #ifndef NECRONDA_SERVER_URI_H
 #define NECRONDA_SERVER_URI_H
 
+#include <sys/stat.h>
+
+typedef struct {
+    char *webroot;
+    char *path;
+    char *pathinfo;
+    char *query;
+    char *filename;
+    char *uri;
+    struct stat stat;
+} uri;
+
 #endif //NECRONDA_SERVER_URI_H
