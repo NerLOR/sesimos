cc53b43c8a2b5f5c1ccb01165be1975ca4e7ab9e
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2020-12-13 14:02:36 +0100

    Added uri.h and uri.c

---
+++ src/uri.c
@@ -0,0 +1,8 @@
+/**
+ * Necronda Web Server
+ * URI and path handlers
+ * src/uri.c
+ * Lorenz Stechauner, 2020-12-13
+ */
+
+#include "uri.h"
---
+++ src/uri.h
@@ -0,0 +1,11 @@
+/**
+ * Necronda Web Server
+ * URI and path handlers (header file)
+ * src/uri.h
+ * Lorenz Stechauner, 2020-12-13
+ */
+
+#ifndef NECRONDA_SERVER_URI_H
+#define NECRONDA_SERVER_URI_H
+
+#endif //NECRONDA_SERVER_URI_H
