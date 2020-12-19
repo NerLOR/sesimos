f6ac55adafca24bfc09cd4c0cb1d92ce4728eb27
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2020-12-19 17:38:40 +0100

    Added cache.c/.h

---
+++ src/cache.c
@@ -0,0 +1,8 @@
+/**
+ * Necronda Web Server
+ * File cache implementation
+ * src/cache.c
+ * Lorenz Stechauner, 2020-12-19
+ */
+
+#include "cache.h"
---
+++ src/cache.h
@@ -0,0 +1,11 @@
+/**
+ * Necronda Web Server
+ * File cache implementation (header file)
+ * src/cache.h
+ * Lorenz Stechauner, 2020-12-19
+ */
+
+#ifndef NECRONDA_SERVER_CACHE_H
+#define NECRONDA_SERVER_CACHE_H
+
+#endif //NECRONDA_SERVER_CACHE_H
