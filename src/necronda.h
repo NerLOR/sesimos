7d6fa4682dd441768757ae70f4e0f1baab1f70ba
Lorenz Stechauner <lorenz.stechauner@necronda.net>
2022-01-13 15:39:04 +0100

    Update version to 4.5

--- src/necronda.h
+++ src/necronda.h
@@ -8,7 +8,7 @@
 #ifndef NECRONDA_SERVER_NECRONDA_H
 #define NECRONDA_SERVER_NECRONDA_H
 
-#define NECRONDA_VERSION "4.4"
+#define NECRONDA_VERSION "4.5"
 #define SERVER_STR "Necronda/" NECRONDA_VERSION
 #define SERVER_STR_HTML "Necronda&nbsp;web&nbsp;server&nbsp;" NECRONDA_VERSION
 
